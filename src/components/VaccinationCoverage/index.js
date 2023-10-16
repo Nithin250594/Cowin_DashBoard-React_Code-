b30f3bee1a0fba6473d7eb8274e8edd0d9d9b890
Nithin250594 <139442810+Nithin250594@users.noreply.github.com>
2023-10-16 11:13:43 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -31,8 +31,8 @@ const VaccinationCoverage = props => {
           tick={{stroke: '#94a3b8', strokeWidth: 0}}
         />
         <Legend wrapperStyle={{padding: 10}} />
-        <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
-        <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="20%" />
+        <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" radius={[10,10,0,0]} />
+        <Bar dataKey="dose2" name="Dose 2" fill="#f54394" radius={[5,5,0,0]}/>
       </BarChart>
     </div>
   )
